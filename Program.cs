3ec1899378c57137f90826120e98986bfdf88556
Mário Ferreira <marioo.netoo3@gmail.com>
2022-11-04 14:42:01 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundo");
+        Console.WriteLine("M = Minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+    }
+    static void Start(int time)
+    {
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+        Console.Clear();
+        Console.WriteLine("StopWatch finalizado!");
+        Thread.Sleep(2500);
+        Menu();
+    }
+}
